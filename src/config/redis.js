b9ed82bac1bdeb7a2b19cbab8b75b3e8245d16c2
kapilkumar12 <kapilkumar1492003@gmail.com>
2026-05-04 13:40:21 +0530

    redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,8 @@
 const { Redis } = require("@upstash/redis");
 
 const connection = new Redis({
- url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+ url: process.env.REDIS_URL,
+  token: process.env.REDIS_TOKEN,
 });
 
 module.exports = connection;
